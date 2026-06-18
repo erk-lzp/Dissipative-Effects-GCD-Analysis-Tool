8c37b7b467462f684d7a7238adda3bacb9301fbe
erk-lzp <r342e598@wichita.edu>
2026-06-18 10:46:39 -0500

    Update paper1_app_3.py
    Title adjsuted

--- paper1_app_3.py
+++ paper1_app_3.py
@@ -335,7 +335,9 @@ def collect_normalization_inputs(device_type):
 # --- Main ------------------------------------------------------------------
 
 def main():
-    st.title("Dissipative Effects - GCD Analysis Tool")
+    st.title("GCD-\u03b3 Analyzer")
+    st.caption("A tool for energy correction in electrochemical "
+               "energy storage devices")
 
     uploaded_file = st.file_uploader("Upload CSV or Excel file",
                                      type=["csv", "xlsx"])
